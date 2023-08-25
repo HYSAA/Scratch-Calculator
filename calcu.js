977e0a51e42720c2d683d2701873bdbad963a804
HYSA <aj.anunciado@gmail.com>
2023-08-26 05:04:45 +0800

    dfssd

--- calcu.js
+++ calcu.js
@@ -1,8 +1,25 @@
-let buttonEl= document.querySelector("buttons")
+let buttonEl= document.querySelectorAll("buttons")
 let inputID= document.getElementById("result")
 
 
+for(let i=0; i < buttonEl.length;i++)
+{
+buttonEl[i].addEventListener("click",() =>{
+let buttonValue=buttonEl[i].textContent;
+console.log(buttonEl[i])
 
+if(buttonValue==="C"){
+    clearResult()
+}else if(buttonValue==="=")
+{
+    calculateResult()
+}
+else{
+appendValue()
+}
+});
+
+}
 
 const clearResult= () =>{
  inputID.textContent=""
